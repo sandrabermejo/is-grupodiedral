12d17caf81b9a07ed25d00e7e14c906247ef1e47
JustAnotherUserName <JustAnotherUserName@ed7ca20c-7e9c-4e62-b569-442508da6f89>
2013-04-28 08:43:01 +0000

    Añadida documentación de análisis

---
+++ doc/analisis/diagramas/umldoc.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Comprueba la existencia de Inkscape antes de empezar
+if (type inkscape > /dev/null)
+then
+	echo "Usando '`inkscape --version`'"
+else
+	echo "Inkscape no está instalado o no es accesible. Finalizado." > 2
+fi
+
+# Obtiene la lista de archivos .svg del directorio
+dir=`ls *.svg`
+
+# Variable: número de conversiones realizadas
+nConv=0
+
+# Variable: número de conversiones omitidas
+nConvOmitidas=0
+
+# Recorre los archivos .svg conviertiéndolos
+for archivo in $dir;
+do
+	# Genera el nombre del archivo .pdf
+	archivoPdf="`basename $archivo .svg`.pdf"
+
+	# Comprueba que el .svg sea más reciente que el .pdf
+	if [ `stat -c %Y $archivoPdf` -lt `stat -c %Y $archivo` ]
+	then
+		inkscape --export-pdf="`basename $archivo .svg`.pdf" $archivo
+
+		# Comprueba que Inskacape ha terminado correctamente
+		if [ $? = 0 ]
+		then
+			nConv=$(($nConv + 1))
+		else
+			echo "Se produjo un error en la conversión del archivo $archivo." > 2
+		fi
+	else
+		nConvOmitidas=$(($nConvOmitidas + 1))
+	fi
+done;
+
+# Muestra el resultado
+if [ $nConv = 0 ]
+then
+	if [ $nConvOmitidas -gt 0 ]
+	then
+		echo "No ha habido cambios. Nada que hacer."
+	else
+		echo "Ningún archivo encontrado."
+	fi
+else
+	echo "Se han convertido $nConv archivos satisfactoriamente ($nConvOmitidas omitidos)."
+fi
