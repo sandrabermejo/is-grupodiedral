2af50c1507d78f55786ba6306d9cf15b45209d38
wachino <wachino@ed7ca20c-7e9c-4e62-b569-442508da6f89>
2013-05-27 17:39:02 +0000

    añadidas clases Pago, Equipaje, PagoTarjeta, Billete, y GestorDe Ofertas Añadidos también diagramas de secuencia de iniciar pago billetes y mostrar  ofertas

---
+++ uml/ace/128007_7.d
@@ -0,0 +1,23 @@
+format 76
+
+packagecanvas 128007 
+  package_ref 128021 // Gestión Externa
+    xyzwh 9 40 2000 305 259
+end
+packagecanvas 128135 
+  package_ref 128007 // Gestión Interna
+    xyzwh 333 40 2000 445 259
+end
+packagecanvas 128519 
+  package_ref 128135 // Personal
+    xyzwh 348 119 2005 129 127
+end
+packagecanvas 128647 
+  package_ref 128263 // Ventas
+    xyzwh 491 118 2005 127 127
+end
+packagecanvas 128775 
+  package_ref 128391 // Mantenimiento
+    xyzwh 628 124 2005 137 121
+end
+end
