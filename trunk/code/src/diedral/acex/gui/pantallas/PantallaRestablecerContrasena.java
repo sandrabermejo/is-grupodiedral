35235c06d9e8891b2d19f445ec77595523c169ec
sandruky <sandruky@ed7ca20c-7e9c-4e62-b569-442508da6f89>
2013-05-30 17:57:10 +0000

    PantallaAcceso con redirecion al olvido de contraseña

---
+++ trunk/code/src/diedral/acex/gui/pantallas/PantallaRestablecerContrasena.java
@@ -0,0 +1,23 @@
+/*
+ * PantallaRestablecerContrasena.java - ACE Gestión Externa - Grupo Diedral 2013
+ */
+package diedral.acex.gui.pantallas;
+
+import javax.swing.JButton;
+
+import diedral.acex.gui.Pantalla;
+
+public class PantallaRestablecerContrasena extends Pantalla {
+
+	public PantallaRestablecerContrasena() {
+		JButton miSuperBoton = new JButton("Buuuuuuuu!");
+		add(miSuperBoton);
+	}
+
+	
+	public String dameNombre() {
+		return "Restablecer contrasena";
+	}
+		
+	
+}
