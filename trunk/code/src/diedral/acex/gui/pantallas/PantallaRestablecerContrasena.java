ea90b84ad3f3f8b7f58959e9a3e5cd8c1a0b2f03
sandruky <sandruky@ed7ca20c-7e9c-4e62-b569-442508da6f89>
2013-05-30 18:23:07 +0000

    modificaciones en PantallaRestablecerContrasena...

--- trunk/code/src/diedral/acex/gui/pantallas/PantallaRestablecerContrasena.java
+++ trunk/code/src/diedral/acex/gui/pantallas/PantallaRestablecerContrasena.java
@@ -3,21 +3,73 @@
  */
 package diedral.acex.gui.pantallas;
 
-import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import diedral.acex.gui.FabricaPantallas;
+import diedral.acex.gui.ManejadorPantallas;
 import diedral.acex.gui.Pantalla;
 
 public class PantallaRestablecerContrasena extends Pantalla {
 
 	public PantallaRestablecerContrasena() {
-		JButton miSuperBoton = new JButton("Buuuuuuuu!");
-		add(miSuperBoton);
+		//Características ventana
+		setLayout(new BorderLayout());
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		//Variables locales auxiliares
+		Dimension dim;
+		
+		// Añade una entradilla de texto
+		add(new JLabel("Un eMail sera enviado a la direccion de correo con la que esta resgistrado."));
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+		
+		// Crea un cuadro de inserción de nombre con su texto
+		JPanel panelCorreo = new JPanel(new GridLayout(1, 2));
+		panelCorreo.add(new JLabel("Correo"));
+		panelCorreo.add(_correo = new JTextField(""));
+		
+		// Acota el tamaño del cuadro de texto para que no quede raro
+		dim = panelCorreo.getMaximumSize();
+		dim.height = _correo.getPreferredSize().height;
+		panelCorreo.setMaximumSize(dim);
+		panel.add(panelCorreo);
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
 	}
 
 	
 	public String dameNombre() {
 		return "Restablecer contrasena";
 	}
-		
 	
+	// MIEMBROS PRIVADOS
+	
+	/**
+	 * Manejador de pantallas
+	 */
+	private ManejadorPantallas _manejadorPantallas;
+	
+	/**
+	 * Fabrica de pantallas
+	 */
+	private FabricaPantallas _fabrica;
+	
+	/**
+	 * Campo de correo electronico (contacto)
+	 */
+	private JTextField _correo;
+		
+	/**
+	 * Valor por defecto del espacio vertical entre componentes
+	 */
+	private static final int INTERESPACIO_VERTICAL = 10;	
 }
