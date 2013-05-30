57ffb89acda224bc6c9b3eded17409795ceeda25
sandruky <sandruky@ed7ca20c-7e9c-4e62-b569-442508da6f89>
2013-05-30 01:26:23 +0000

    Pantalla de registrarse

---
+++ code/src/diedral/acex/gui/pantallas/PantallaRegistro.java
@@ -0,0 +1,191 @@
+/*
+ * PaginaRegistro.java - ACE Gestion Externa - Grupo diedral 2013
+ */
+package diedral.acex.gui.pantallas;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import diedral.acex.GestorUsuarios;
+import diedral.acex.Sesion;
+import diedral.acex.Usuario;
+import diedral.acex.gui.FabricaPantallas;
+import diedral.acex.gui.ManejadorPantallas;
+import diedral.acex.gui.Pantalla;
+
+public class PantallaRegistro extends Pantalla {
+	/**
+	 * Constuctor
+	 */
+	public PantallaRegistro() {
+		
+		//Caracteristicas ventana
+		setLayout(new BorderLayout());
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		//Variables locales auxiliares
+		Dimension dim;
+		
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+		
+		// Crea un cuadro de insercion de NOMBRE con su texto
+		JPanel panelNombre = new JPanel(new GridLayout(1, 2));
+		panelNombre.add(new JLabel("Nombre"));
+		panelNombre.add(_nombre = new JTextField(""));
+		
+		// Acota el tamanio del cuadro de texto para que no quede raro
+		dim = panelNombre.getMaximumSize();
+		dim.height = _nombre.getPreferredSize().height;
+		panelNombre.setMaximumSize(dim);
+		panel.add(panelNombre);
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+		
+		// Crea un cuadro de insercion de PRIMER APELLIDO con su texto
+		JPanel panelPrimerApellido = new JPanel(new GridLayout(1, 2));
+		panelPrimerApellido.add(new JLabel("Primer Apellido"));
+		panelPrimerApellido.add(_apellido1 = new JTextField(""));
+		
+		// Acota el tamanio del cuadro de texto para que no quede raro
+		panelPrimerApellido.setMaximumSize(dim);
+		panel.add(panelPrimerApellido);
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+		
+		// Crea un cuadro de insercion de SEGUNDO APELLIDO con su texto
+		JPanel panelSegundoApellido = new JPanel(new GridLayout(1, 2));
+		panelSegundoApellido.add(new JLabel("Segundo Apellido"));
+		panelSegundoApellido.add(_apellido2 = new JTextField(""));
+		
+		// Acota el tamanio del cuadro de texto para que no quede raro
+		panelSegundoApellido.setMaximumSize(dim);
+		panel.add(panelSegundoApellido);
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+		
+		// Crea un cuadro de insercion de CORREO con su texto
+		JPanel panelCorreo = new JPanel(new GridLayout(1, 2));
+		panelCorreo.add(new JLabel("Correo"));
+		panelCorreo.add(_correo = new JTextField(""));
+		
+		// Acota el tamanio del cuadro de texto para que no quede raro
+		dim = panelCorreo.getMaximumSize();
+		dim.height = _correo.getPreferredSize().height;
+		panelCorreo.setMaximumSize(dim);
+		panel.add(panelCorreo);
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+				
+		// Crea un cuadro de insercion de CONTRASENA
+		JPanel panelContrasena = new JPanel(new GridLayout(1, 2));
+		panelContrasena.add(new JLabel("Contrasena"));
+		panelContrasena.add(_contrasena = new JPasswordField(""));
+		
+		// Acota el tamanio del cuadro de texto para que no quede raro
+		panelContrasena.setMaximumSize(dim);
+		panel.add(panelContrasena);
+				
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+		panel.add(Box.createVerticalStrut(INTERESPACIO_VERTICAL));
+		
+		JButton registrarse = new JButton("Registrarse");
+		registrarse.addActionListener(new Registrarse());
+		panel.add(registrarse);
+		
+		add(panel);
+	}
+	
+	/**
+	 * Devuelve el nombre de la ventana
+	 */
+	public String dameNombre(){
+		return "Registrarse";
+	}
+	
+	@Override
+	public void estableceContexto(ManejadorPantallas manejador, FabricaPantallas fabrica, Sesion sesion) {
+		_mnj = manejador;
+		_sesion = sesion;
+	}
+	
+	/**
+	 * Clase oyente para iniciar sesion
+	 *
+	 */
+	private class Registrarse implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			Usuario usuario = GestorUsuarios.dameInstancia().buscaUsuario(_correo.getText());
+			
+			if (usuario == null || !usuario.dameContrasena().equals(_contrasena.getText()))
+				JOptionPane.showMessageDialog(PantallaRegistro.this,
+						"Datos no validos.",
+						"ACE Gestion Externa - Acceso",
+						JOptionPane.ERROR_MESSAGE);
+			else {
+				_sesion.cargaUsuario(usuario);
+				_mnj.cierraPantallaActual();
+			}
+								
+		}
+		
+	}
+	// MIEMBROS PRIVADOS
+	
+	/**
+	 * Manejador de pantallas
+	 */
+	private ManejadorPantallas _mnj;
+	
+	/**
+	 * Sesion actual
+	 */
+	private Sesion _sesion;
+
+	/**
+	 * Campo de nombre
+	 */
+	private JTextField _nombre;
+
+	/**
+	 * Campo de apellido1
+	 */
+	private JTextField _apellido1;
+
+	/**
+	 * Campo de apellido2
+	 */
+	private JTextField _apellido2;
+	
+	/**
+	 * Campo de correo electronico (contacto)
+	 */
+	private JTextField _correo;
+	
+	/**
+	 * Campo de contrasena
+	 */
+	private JPasswordField _contrasena;
+	
+	/**
+	 * Valor por defecto del espacio vertical entre componentes
+	 */
+	private static final int INTERESPACIO_VERTICAL = 10;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7267686888267890536L;
+	
+}
